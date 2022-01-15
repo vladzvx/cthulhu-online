ba97b5e3634f2825362d445f6f0bb70c3e008930
vladzvx <wladzwer@mail.ru>
2022-01-16 00:29:25 +0300

    first commit

---
+++ Cthulhu.CoreLib/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Cthulhu.CoreLib
+{
+    public class Class1
+    {
+    }
+}
---
+++ Cthulhu.Service/Controllers/TestController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cthulhu.Service.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        public TestController()
+        {
+
+        }
+
+        [HttpPost()]
+        public async Task<string> Test()
+        {
+            return "ok";
+        }
+    }
+}
